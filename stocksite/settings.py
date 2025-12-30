23efb94b753742890f0e1b1add29620aa33f74c1
Stones004 <166741643+Stones004@users.noreply.github.com>
2025-12-30 21:34:24 +0530

    Switch database to SQLite

--- stocksite/settings.py
+++ stocksite/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'stocksite.wsgi.application'
     }
 } '''
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
@@ -101,8 +101,14 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
-}
+} '''
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 
